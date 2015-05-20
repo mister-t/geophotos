72df7e71b5f4a78621d687e7ca5faed22686ac5e
Tony Yeung <mr.tyeung+new_mac_2014@gmail.com>
2015-05-20 14:45:32 -0700

    Added grunt-contrib-requirejs plugin to handle minifying the js files

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,29 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    requirejs: {
+      desktopJS: {
+        options: {
+          baseUrl: "public/js/app",
+          wrap: true,
+          // Cannot use almond since it does not currently appear to support requireJS's config-map
+          //name: "../libs/almond",
+          preserveLicenseComments: false,
+          optimize: "uglify",
+          mainConfigFile: "public/js/app/config/config.js",
+          include: ["init/DesktopInit"],
+          name: "init/DesktopInit",
+          out: "public/js/app/init/DesktopInit.min.js"
+        }
+      },
+      desktopCSS: {
+        options: {
+          optimizeCss: "standard",
+          cssIn: "./public/css/app.css",
+          out: "./public/css/app.min.css"
+        }
+      }
+    },
     jshint: {
       files: ['Gruntfile.js', 'public/js/app/**/*.js', '!public/js/app/**/*min.js'],
       options: {
@@ -52,6 +75,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('test', ['jshint']);
-  grunt.registerTask('build', ['jshint']);
+  grunt.registerTask('build', ['desktopJS', 'desktopCSS']);
   grunt.registerTask('default', ['test', 'build']);
 };
