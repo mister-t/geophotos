ebfc02594826ee76d55882abb687c955b9c22917
Tony Yeung <mr.tyeung+new_mac_2014@gmail.com>
2015-05-20 15:22:49 -0700

    Added nodemon and concurrent Grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,6 +46,19 @@ module.exports = function(grunt) {
         }
       }
     },
+    nodemon: {
+      dev: {
+        script: './bin/www'
+      }
+    },
+    concurrent: {
+      dev: {
+        tasks: ['nodemon', 'watch'],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
+    },
     watch: {
       options: {
         livereload: true
@@ -73,8 +86,16 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-concurrent');
+
+
+  grunt.registerTask('build:debug', 'Lint and compile', [
+    'jshint', 'less:dev'
+  ]);
 
   grunt.registerTask('test', ['jshint']);
   grunt.registerTask('build', ['requirejs:desktopJS', 'requirejs:desktopCSS']);
   grunt.registerTask('default', ['test', 'build']);
+  grunt.registerTask('dev', ['build:debug', 'concurrent']);
 };
