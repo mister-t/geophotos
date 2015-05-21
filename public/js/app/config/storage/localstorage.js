1c19b39199d9fc0825fa4e3956cdbe1eeed29036
Tony Yeung <mr.tyeung+new_mac_2014@gmail.com>
2015-05-21 10:32:58 -0700

    Reverted the localstorage back to previous state in order to be compatible with older libs

--- public/js/app/config/storage/localstorage.js
+++ public/js/app/config/storage/localstorage.js
@@ -1,49 +1,33 @@
 define(['App', 'backbone.localstorage'], function (App) {
   App.module('Entities', function (Entities, App, Backbone, Marionette, $, _) {
-    var findStorageKey = function(entity) {
+    var findStorageKey = function (entity) {
       // use a model's urlRoot value
-      if (entity.urlRoot) {
-        return _.result(entity, "urlRoot");
+      if(entity.urlRoot) {
+        return _.result(entity, 'urlRoot');
       }
       // use a collection's url value
-      if (entity.url) {
-        return _.result(entity, "url");
+      if(entity.url) {
+        return _.result(entity, 'url');
+      }
+      // fallback to obtaining a model's storage key from
+      // the collection it belongs to
+      if(entity.collection && entity.collection.url) {
+        return _.result(entity.collection, 'url');
       }
 
       throw new Error("Unable to determine storage key");
     };
 
-    var StorageMixin = function(entityPrototype) {
+    var StorageMixin = function (entityPrototype) {
       var storageKey = findStorageKey(entityPrototype);
-      return {
-        localStorage: new Backbone.LocalStorage(storageKey)
-      };
+      return { localStorage: new Backbone.LocalStorage(storageKey) };
     };
 
-    var getEntity = function(constructorString) {
-      var sections = constructorString.split("."),
-          entity = window;
-
-      _.each(sections, function(section) {
-          entity = entity[section];
-      });
-
-      return entity;
-    };
-
-    Entities.configureStorage = function(constructorString) {
-      var OldConstructor = getEntity(constructorString);
-      var NewConstructor = function() {
-        var obj = new OldConstructor(arguments[0], arguments[1]);
-        _.extend(obj, new StorageMixin(OldConstructor.prototype));
-
-        return obj;
-      }
-      NewConstructor.prototype = OldConstructor.prototype;
-
-      eval(constructorString + " = NewConstructor;");
+    Entities.configureStorage = function (entity) {
+      _.extend(entity.prototype, new StorageMixin(entity.prototype));
     };
   });
 
   return App.Entities.configureStorage;
 });
+
