2167ee7d8220607033282fbf57a1d9191bddefbc
Tony Yeung <mr.tyeung+new_mac_2014@gmail.com>
2015-05-21 00:23:15 -0700

    Added a student profile to the student entities

--- public/js/app/entities/students.js
+++ public/js/app/entities/students.js
@@ -57,10 +57,48 @@ define([ 'App', 'config/storage/localstorage'], function (App) {
 
     var initializeStudents = function() {
       var students = new Entities.StudentCollection([
+        {
+          "id" : 20770,
+          "sex" : "F",
+          "primeCurric" : "5752",
+          "firstname" : "uaYa osainyD",
+          "lastname" : "nUmee",
+          "emailaddr" : "uaYaosainyD.nUmee@test.com",
+          "termEnrollmentsForEnlisted": [{
+            "calres" : "R",
+            "entstat" : "C",
+            "primecurr" : "P",
+            "calendarForAppliesTo" : {
+              "id" : "WI09"
+            },
+            "curriculumForEnrolledIn" : {
+              "id" : "5750",
+              "name" : "Physical Therapy (MS)   "
+            },
+            "studyListsForParentCurric": [{
+              "id" : 1681385,
+              "course" : "122.01",
+              "name" : "Mammalian Physiology II",
+              "ucode" : "PY122.010 "
+            }, {
+              "id" : 1681383,
+              "course" : "110",
+              "name" : "Ortho & Rehab Diag/Treatment",
+              "ucode" : "PT1100    "
+            }, {
+              "id" : 1681384,
+              "course" : "410",
+              "name" : "Clinical Clerkship",
+              "ucode" : "PT4100    "
+            }]
+          }]
+        }
       ]);
+
       students.forEach(function(student) {
           student.save();
       });
+
       return students.models;
     };
 
