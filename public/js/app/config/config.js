8cb16d9a5e0ecf316f9bb6ca87c4c8df1fbcbd63
Tony Yeung <mr.tyeung+new_mac_2014@gmail.com>
2015-05-20 22:06:12 -0700

    Added Lodash library as a replacement for Underscore.js; added require.js configurations for the various libraries we have

--- public/js/app/config/config.js
+++ public/js/app/config/config.js
@@ -2,7 +2,40 @@ require.config({
   baseUrl: "/js/app",
   paths: {
     desktopInit: "init/DesktopInit",
+    jquery: "../libs/jquery",
+    underscore: "../libs/lodash",
+    backbone: "../libs/backbone",
+    marionette: "../libs/backbone.marionette",
+    handlebars: "../libs/handlebars",
+    hbs: "../libs/hbs",
+    i18nprecompile: "../libs/i18nprecompile",
+    json2: "../libs/json2",
+    "backbone.syphon": "../libs/backbone.syphon",
+    "localstorage": "../libs/backbone.localstorage",
   },
   shim: {
+    backbone: {
+      deps: [
+          "underscore", "jquery"
+      ],
+      "exports": "Backbone"
+    },
+    "backbone.syphon": [
+      "backbone"
+    ],
+    "backbone.localstorage": [
+      "backbone"
+    ],
+    marionette: {
+      deps: [
+        "underscore",
+        "backbone",
+        "jquery"
+      ],
+      exports: "Marionette"
+    },
+    handlebars: {
+      exports: "Handlebars"
+    }
   }
 });
