01ca42c845688ea28f1ce3d4f95771066364c9ae
Tony Yeung <mr.tyeung+new_mac_2014@gmail.com>
2015-08-24 14:24:12 -0700

    Added photo list views and photo template

---
+++ public/js/app/photos/list/ListView.js
@@ -0,0 +1,65 @@
+define([
+  'App',
+  'constants',
+  'jquery',
+  'toastr',
+  'hbs!photos/list/templates/heading',
+  'hbs!photos/list/templates/photo',
+  'hbs!photos/list/templates/photos'
+], function(App, Constants, $, toastr, headingTpl, photoTpl, photosTpl) {
+
+  App.module('PhotosApp.List.View', function(View, App, Backbone, Marionette, $, _){
+    View.Heading = Backbone.Marionette.ItemView.extend({
+
+      template: headingTpl,
+
+      events: {
+      },
+
+      initialize: function (options) {
+        this.model = new Backbone.Model();
+      },
+
+      onRender: function () {
+      }
+    });
+
+    View.Photo = Backbone.Marionette.ItemView.extend({
+
+      className: 'col-lg-4',
+
+      template: photoTpl,
+
+      events: {
+      },
+
+      showProfile: function (evt) {
+        evt.preventDefault();
+      },
+
+      onRender: function () {
+      }
+    });
+
+    View.PhotoList = Backbone.Marionette.CompositeView.extend({
+      id: 'photo_list',
+
+      template: photosTpl,
+
+      itemViewContainer: '#photo_list_results',
+
+      itemView: View.Photo,
+
+      events: {
+      },
+
+      initialize: function (options) {
+      },
+
+      onRender: function () {
+      }
+    });
+  });
+
+  return App.PhotosApp.List.View;
+});
