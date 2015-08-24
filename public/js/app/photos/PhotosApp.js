8250b023dcbc06821234ccb28b0d932585422028
Tony Yeung <mr.tyeung+new_mac_2014@gmail.com>
2015-08-24 14:42:26 -0700

    Added the PhotosApp sub-app to take care of listening to messages specific to the photos subapp

---
+++ public/js/app/photos/PhotosApp.js
@@ -0,0 +1,33 @@
+define([
+  'App',
+  'constants',
+  'photos/list/ListController'
+], function (App, Constants, ListController) {
+  App.module('PhotosApp', function (PhotosApp, App, Backbone, Marionette, $, _) {
+    PhotosApp.Router = Marionette.AppRouter.extend({
+      appRoutes: {
+        'photos'    : 'showPhotos'
+      }
+    });
+
+    var
+      API = {
+        showPhotos: function (query) {
+          App.navigate('/photos');
+          ListController.showPhotos(query);
+        }
+      };
+
+    App.on(Constants.photos.list, function (query) {
+      API.showPhotos(query);
+    });
+
+    App.addInitializer(function () {
+      new PhotosApp.Router({
+        controller: API
+      });
+    });
+  });
+
+  return App.PhotosApp;
+});
