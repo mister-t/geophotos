a1750aea7dda7f1b75779b5ea0cceb82c34e53ec
Tony Yeung <mr.tyeung+new_mac_2014@gmail.com>
2015-08-26 02:37:55 -0700

    Modularized some more code

--- routes/instagram.js
+++ routes/instagram.js
@@ -7,42 +7,17 @@ var
 
 Bluebird.promisifyAll(instaApi);
 
-
-/* Index Page
- * IF the instagram cookie is present, show the app
- * ELSE show an Instagram login button
- */
-router.get('/', function (req, res) {
-  if (req.cookies.instaToken) {
-    console.log('instagram token cookie deteced');
-    instaApi.use({ access_token: req.cookies.instaToken });
-    return instaApi.media_searchAsync(config.cities.sf.lat, config.cities.sf.lng)
-    .spread(function (medias, pagination, remaining, limit) {
-      return medias;
-    })
-    .then(function (medias) {
-      console.log(medias);
-      res.render('index', config.params);
-    })
-    .catch(function (errors) {
-      console.log(errors);
+var getGrams = function (medias) {
+  var results = [];
+  medias.forEach(function (m) {
+    results.push({
+      imgSrc: m.images.thumbnail.url,
+      caption: m.caption && m.caption.text ? m.caption.text : ''
     });
-  } else {
-    console.log('instagram token cookie not deteced');
-    //res.render('index', config.params);
-    res.redirect('/authorize-user');
-  }
-});
-
-
-/* Redirect user to Instagram for authentication */
-router.get('/authorize-user', function (req, res) {
-  instaApi.use({
-    client_id: config.instagram_client_id,
-    client_secret: config.instagram_client_secret
   });
-  res.redirect(instaApi.get_authorization_url(config.instagram_redirect_uri));
-});
+
+  return results;
+};
 
 var handleAuth = function (req, res) {
   instaApi.authorize_userAsync(req.query.code, config.instagram_redirect_uri)
@@ -55,9 +30,43 @@ var handleAuth = function (req, res) {
   });
 };
 
+/* check to see if instagram token exists.
+* if yes, display the index page.
+* if no, log in first, then display the index page*/
+router.get('/', function (req, res) {
+  if (req.cookies.instaToken) {
+    console.log('instagram token cookie deteced');
+    instaApi.use({ access_token: req.cookies.instaToken });
+    return instaApi.media_searchAsync(config.cities.sf.lat, config.cities.sf.lng)
+    .spread(function (medias, pagination, remaining, limit) {
+      return medias;
+    })
+    .then(function (medias) {
+      console.log(getGrams(medias));
+      res.render('index', config.params);
+    })
+    .catch(function (errors) {
+      console.log(errors);
+    });
+  } else {
+    console.log('instagram token cookie not deteced');
+    //res.render('index', config.params);
+    res.redirect('/authorize-user');
+  }
+});
+
+/* Redirect user to Instagram for authentication */
+router.get('/authorize-user', function (req, res) {
+  instaApi.use({
+    client_id: config.instagram_client_id,
+    client_secret: config.instagram_client_secret
+  });
+  res.redirect(instaApi.get_authorization_url(config.instagram_redirect_uri));
+});
 
 /* Set cookie once Instagram sends access code */
 router.get('/handleauth', handleAuth);
 router.get(config.instagram_redirect_partial_uri, handleAuth);
+
 
 module.exports = router;
